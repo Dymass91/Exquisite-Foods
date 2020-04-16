03e2d12d6d62c993e0061e223b39fd11aad42b34
Tomasz Matyszczak <Matyszczak24@gmail.com>
2020-04-16 13:31:17 +0100

    Updates

---
+++ precache-manifest.b15e2af0c7ea4c526d800309d198c4f2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b01d409f4ceb9d4cc82dbccd9958b90",
+    "url": "/Exquisite-Foods/index.html"
+  },
+  {
+    "revision": "132b6564662e5b5079fe",
+    "url": "/Exquisite-Foods/static/css/main.331385cc.chunk.css"
+  },
+  {
+    "revision": "0ba7d523f3eb97446656",
+    "url": "/Exquisite-Foods/static/js/2.bf23ac5f.chunk.js"
+  },
+  {
+    "revision": "132b6564662e5b5079fe",
+    "url": "/Exquisite-Foods/static/js/main.efe3fd2b.chunk.js"
+  },
+  {
+    "revision": "9db18fda0b241be75ff7",
+    "url": "/Exquisite-Foods/static/js/runtime~main.6c0a4274.js"
+  },
+  {
+    "revision": "87ecaba89eb46b86038b927fc8c95f1f",
+    "url": "/Exquisite-Foods/static/media/Extravirginoil.87ecaba8.jpg"
+  },
+  {
+    "revision": "8cc80d97ddb56ef0eea253f16d080892",
+    "url": "/Exquisite-Foods/static/media/Extravirginoilsmall.8cc80d97.jpg"
+  },
+  {
+    "revision": "3c6b361caed77b2ff1f49af79fe64e20",
+    "url": "/Exquisite-Foods/static/media/MainImg4.3c6b361c.jpg"
+  },
+  {
+    "revision": "91935aedfec5e557c96db0c74c38b355",
+    "url": "/Exquisite-Foods/static/media/MainImg5.91935aed.jpg"
+  },
+  {
+    "revision": "18b2aa5dfcef546038ee5ec309c57c0f",
+    "url": "/Exquisite-Foods/static/media/MainImg7.18b2aa5d.jpg"
+  },
+  {
+    "revision": "e5a3af4da4c2930944c05d0b246c27db",
+    "url": "/Exquisite-Foods/static/media/MainImg8.e5a3af4d.jpg"
+  },
+  {
+    "revision": "cebeed562b34f12c5435fc6bcc2f7750",
+    "url": "/Exquisite-Foods/static/media/benefitsimg.cebeed56.jpg"
+  },
+  {
+    "revision": "8998694fa8ddfd701e2a79c7f7a8c80a",
+    "url": "/Exquisite-Foods/static/media/header3.8998694f.jpeg"
+  },
+  {
+    "revision": "d354758ad85515f36fa1720e0e2739ba",
+    "url": "/Exquisite-Foods/static/media/olej4_5.d354758a.jpg"
+  },
+  {
+    "revision": "c483d1e576f83917687667fb2e05b000",
+    "url": "/Exquisite-Foods/static/media/olej5l.c483d1e5.jpg"
+  },
+  {
+    "revision": "d2992b39e27ab9bcab2474af28fe25dc",
+    "url": "/Exquisite-Foods/static/media/olejcan.d2992b39.jpg"
+  },
+  {
+    "revision": "d0b1a0f8cddbbbd82d93c9e40a3f6d3c",
+    "url": "/Exquisite-Foods/static/media/olive-oil-head.d0b1a0f8.jpg"
+  },
+  {
+    "revision": "b9bcb1c3181aacaebeb4e024074ad3b6",
+    "url": "/Exquisite-Foods/static/media/olives2.b9bcb1c3.jpg"
+  },
+  {
+    "revision": "2901b07707c8eb01e9fbe204562066f3",
+    "url": "/Exquisite-Foods/static/media/town.2901b077.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Exquisite-Foods/precache-manifest.3e62fb8283ba99508bd50447f9cc9589.js"
+  "/Exquisite-Foods/precache-manifest.b15e2af0c7ea4c526d800309d198c4f2.js"
 );
 
 self.addEventListener('message', (event) => {
