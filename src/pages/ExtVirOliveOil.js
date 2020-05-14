eb36c04ae71e0fca9683437abcfd1db206513e0e
Tomasz Matyszczak <Matyszczak24@gmail.com>
2020-05-14 19:03:20 +0100

    new text to sections

--- src/pages/ExtVirOliveOil.js
+++ src/pages/ExtVirOliveOil.js
@@ -11,15 +11,53 @@ const ExtOlivePage = () => {
         <>
             <div className="oil_products-list">
                 <h1 className="oil_product_list-h1">Extra virgin olive oil</h1>
+                <div className='OilIntroduce_text'>
+                    Dietary fats are highly controversial, with debates about animal fats, seed oils, and everything in between in full force.
+                <br></br><br></br>
+                That said, most people agree that extra virgin olive oil is incredibly healthy.
+                <br></br><br></br>
+                Part of the Mediterranean diet, this traditional oil has been a dietary staple for some of the world's healthiest populations.
+                <br></br><br></br>
+                Studies show that the fatty acids and antioxidants in olive oil can offer some powerful health benefits, including a reduced risk of heart disease.
+                <br></br><br></br>
+                This article reviews why extra virgin olive oil is one of the healthiest fats.
+                <br></br><br></br>
+                Olive oil is oil that has been extracted from olives, the fruits of the olive tree.
+                <br></br><br></br>
+                The production process is incredibly simple. Olives can be pressed to extract their oil, but modern methods involve crushing the olives, mixing them together, and then separating the oil from the pulp in a centrifuge.
+                <br></br><br></br>
+                After centrifugation, small amounts of oil remain in the pomace. The leftover oil can be extracted using chemical solvents and is known as olive pomace oil.
+                <br></br><br></br>
+                Olive pomace oil is generally cheaper than regular olive oil and has a bad reputation.
+                <br></br><br></br>
+                Buying the right typeof olive oil is crucial. There are three main grades of olive oil — refined, virgin, and extra virgin. Extra virgin olive oil is the least processed or refined type.
+                <br></br><br></br>
+                Extra virgin olive oil is considered to be the healthiest type of olive oil. It’s extracted using natural methods and standardized for purity and certain sensory qualities like taste and smell.
+                <br></br><br></br>
+                Olive oil that is truly extra virgin has a distinct taste and is high in phenolic antioxidants, which is the main reason why it’s so beneficial.
+        </div>
+
+
                 <div className="box">
                     <div className="imgbox">
                         <img className="oil-image" src={extravirginoil} alt="" />
                     </div>
                     <div className="content">
-                        <h2>Extra virgin oil - 5 litres can</h2>
-                        <p>Can of 5 liters of oil with denomination of origin "Sierra de Cádiz". This modern format stands out for its convenience and ease of use and storage, and for the denomination of origin "Sierra de Cádiz".</p>
-                        <h4>Format: Box of 6 cans</h4>
-                        <h4>Price: Ask about</h4>
+                        <h2>FORMAT 500 ml </h2>
+
+                        <h4>
+                            1+ x £ 5.00
+                        <br></br>
+15+ x £ 4.50
+<br></br>
+100+ x £ 4.00
+<br></br>
+200+ x £ 3.50
+<br></br>
+350+ x £ 3.00
+<br></br>
+500+ x £ 2.70
+                        </h4>
                     </div>
 
                 </div>
@@ -30,10 +68,21 @@ const ExtOlivePage = () => {
                         <img className="oil-image" src={extravirginoil} alt="" />
                     </div>
                     <div className="content">
-                        <h2>Extra virgin oil - 5 litres</h2>
-                        <p>5 liter bottle of extra virgin olive oil. Probably, this is the format preferred by housewives, because it is the cheapest and also because it has more quantity.</p>
-                        <h4>Format: Box of 4 bottles</h4>
-                        <h4>Price: Ask about</h4>
+                        <h2>FORMAT 750 ml. </h2>
+
+                        <h4>
+                            1+ x £ 6.00
+                            <br></br>
+15+ x £ 5.50
+<br></br>
+100+ x £ 5.00
+<br></br>
+200+ x £ 4.50
+<br></br>
+350+ x £ 4.00
+<br></br>
+500+ x £ 3.70
+</h4>
                     </div>
 
                 </div>
@@ -43,10 +92,22 @@ const ExtOlivePage = () => {
                         <img className="oil-image" src={extravirginoil} alt="" />
                     </div>
                     <div className="content">
-                        <h2>Extra virgin oil - 3/4 litres</h2>
-                        <p>Our flagship product, packaged in the best package of all time, the glass, to keep all its fragrance and flavor. Specially manufactured, packaged raw (unfiltered) to satisfy the most demanding palates.</p>
-                        <h4>Format: Box of 15 bottles.</h4>
-                        <h4>Price: Ask about</h4>
+                        <h2>FORMAT 5 litres. </h2>
+
+                        <h4>
+                            1+ x £ 30.00
+                            <br></br>
+                            4+ x £ 28.00
+                            <br></br>
+                            10+ x £ 25.00
+                            <br></br>
+                            20+ x £ 23.00
+                            <br></br>
+                            50+ x £ 21.00
+                            <br></br>
+                            100+ x £ 19.00
+                        </h4>
+
                     </div>
 
                 </div>
--- src/pages/JamsonOil.js
+++ src/pages/JamsonOil.js
@@ -2,14 +2,35 @@ import React from 'react';
 import "../styles/Eco&Ext_Oil.css";
 import olejcan from '../images/olejcan.jpg';
 import extravirginoil from '../images/Extravirginoil.jpg';
-import extravirginoilsmall from '../images/Extravirginoilsmall.jpg';
+import JamonLogo from '../Jamon Files/LOGO OIL JAMON - TRANS.png';
+
 
 
 const ExtOlivePage = () => {
     return (
         <>
             <div className="oil_products-list">
+
                 <h1 className="oil_product_list-h1">Oil and Jamón</h1>
+
+                <div className="OilIntroduce_text">
+                    {/* <img className="oil-Jamon" src={JamonLogo} alt="" /> */}
+                    <strong>Oil&Jamón</strong>  is a new brand, created in January 2017 in Sevilla, Spain. After months of trial, we succeeded to blend, through a traditional process, Iberian ham and extra virgin olive oil, to produce a unique condiment that brings flavor and aroma to our home-made dishes.
+<br></br><br></br>
+                    Our company, <strong>Alimentación</strong> e  <strong>Innovación Jama S.L.</strong> , was born out of our love for traditional cuisine and prime quality raw materials, the basis of our corporate philosophy.
+                    <br></br><br></br>
+                    Our manufacturing process is entirely traditional and respectful of all quality standards applicable. We use exclusively Andalusian goods: Iberian ham from the Sierra de Aracena, in the province of Huelva, and extra virgin olive oil. A month of maceration allows the perfect blend of flavors from these two distinctive products of the Spanish gastronomy.
+                    With our products <strong>Oil&Jamón</strong> come to fruition the two defining concepts of our brand: <strong>Innovation</strong> and <strong>tradition</strong>.
+                    <br></br><br></br>
+                    Our oil <strong>Oil&Jamón</strong> amply deserves its name of condiment, as it blends harmoniously with all sorts of dishes: white and red meats, salted cod and tuna, omelets, vegetables, mushrooms, pasta, lentils, mashed potatoes, etc. Nothing like it to bring out the flavors of canned food or any other dish of our choice.
+                    <br></br><br></br>
+                    With a bottle of 25 cl, we can season more than 100 dishes. Remember that Oil&Jamón isn’t made for cooking, it is added to our dishes to give them a unique flavor.
+                    <br></br><br></br>
+                    Thank you for betting on a healthy and innovative product.
+
+
+                </div>
+
                 <div className="box">
                     <div className="imgbox">
                         <img className="oil-image" src={extravirginoil} alt="" />
